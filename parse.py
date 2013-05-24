bac353fec5a382886b645979ffbf7c2cdb6b9cea
Delanir <danielafilipa@gmail.com>
2013-05-24 20:43:57 +0100

    parse DB

---
+++ parse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+from string import letters
+
+#http://opengeocode.org/download/cow.php
+
+f = open('cow.txt', 'r')
+
+listOfLines=list()
+
+for line in f:
+	
+	if line[0]!='#':
+		listOfLines.append(line)
+f.close()
+
+
+minLatitude=list()
+maxLatitude=list()
+countries = list()
+countriesExt = list()
+
+
+for line in listOfLines:
+	aux= line.split(';')
+	if len(aux)==1 or aux[-7]=="minlatitude":
+		pass
+	else:
+		minLatitude.append(float(aux[-7]))
+		maxLatitude.append(float(aux[-8]))
+		countries.append(aux[0])
+		countriesExt.append(aux[4])
+
+
+	
+
+a= raw_input()
+
+latitude= float(a)
+
+for indexCountry in range(len(countries)):
+	if latitude >= minLatitude[indexCountry] and latitude <=maxLatitude[indexCountry]:
+		print countriesExt[indexCountry]
+
+
